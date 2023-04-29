524e76645e36740ca696ee0d8b8c40b9978d2736
RanaMaj <ranamaj2002@gmail.com>
2023-04-29 14:36:16 +0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,76 @@
-var maxLength = 83;
-var myParagraph = document.getElementById("myParagraph");
-var text = myParagraph.textContent;
+// $(".image-magnafic").magnificPopup({
+//   type: "image",
+//   gallery: {
+//     enabled: true,
+//   },
+// });
 
-if (text.length > maxLength) {
-  var shortenedText = text.substring(0, maxLength) + "...";
-  myParagraph.textContent = shortenedText;
-}
+// $(".video-magnafic").magnificPopup({
+//   type: "iframe",
+//   iframe: {
+//     markup:
+//       '<div class="mfp-iframe-scaler">' +
+//       '<div class="mfp-close"></div>' +
+//       '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+//       "</div>",
+
+//     patterns: {
+//       youtube: {
+//         index: "youtube.com/",
+
+//         id: "v=",
+
+//         src: "//www.youtube.com/embed/%id%?autoplay=1",
+//       },
+//       vimeo: {
+//         index: "vimeo.com/",
+//         id: "/",
+//         src: "//player.vimeo.com/video/%id%?autoplay=1",
+//       },
+//       gmaps: {
+//         index: "//maps.google.",
+//         src: "%id%&output=embed",
+//       },
+//     },
+
+//     srcAction: "iframe_src",
+//   },
+// });
+
+// var maxLength = 83;
+// var myParagraph = document.getElementById("myParagraph");
+// var text = myParagraph.textContent;
+
+// if (text.length > maxLength) {
+//   var shortenedText = text.substring(0, maxLength) + "...";
+//   myParagraph.textContent = shortenedText;
+// }
 
 // --------------------------------------------
-var maxLength = 83;
-var myParagraph2 = document.getElementById("myParagraph2");
-var text = myParagraph2.textContent;
+// var maxLength = 83;
+// var myParagraph2 = document.getElementById("myParagraph2");
+// var text = myParagraph2.textContent;
 
-if (text.length > maxLength) {
-  var shortenedText = text.substring(0, maxLength) + "...";
-  myParagraph2.textContent = shortenedText;
-}
+// if (text.length > maxLength) {
+//   var shortenedText = text.substring(0, maxLength) + "...";
+//   myParagraph2.textContent = shortenedText;
+// }
 
 // --------------------------------------------
-var maxLength = 83;
-var myParagraph3 = document.getElementById("myParagraph3");
-var text = myParagraph3.textContent;
+// var maxLength = 83;
+// var myParagraph3 = document.getElementById("myParagraph3");
+// var text = myParagraph3.textContent;
 
-if (text.length > maxLength) {
-  var shortenedText = text.substring(0, maxLength) + "...";
-  myParagraph3.textContent = shortenedText;
-}
+// if (text.length > maxLength) {
+//   var shortenedText = text.substring(0, maxLength) + "...";
+//   myParagraph3.textContent = shortenedText;
+// }
 
 // console.log("Number of characters in <h3>: " + text.length);
 
 // Count Time JS
 function makeTimer() {
-  var endTime = new Date("november  30, 2021 17:00:00 PDT");
+  var endTime = new Date("november  30, 2024 17:00:00 PDT");
   var endTime = Date.parse(endTime) / 1000;
   var now = new Date();
   var now = Date.parse(now) / 1000;
@@ -59,10 +98,3 @@ function makeTimer() {
 setInterval(function () {
   makeTimer();
 }, 300);
-
-// Dark Mode
-
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
